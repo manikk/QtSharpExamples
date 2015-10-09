f78d83b2a4c38ae889ff7b96d4067fbd1300aa2c
manikk <manikk.h@gmail.com>
2015-10-09 22:27:28 +0530

    ZetCode Quit Button Example added

---
+++ ZetCodeExamples/QuitButton/QuitButtonExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using QtGui;
+using QtWidgets;
+
+namespace QuitButton
+{
+	public class QuitButtonExample:QWidget
+	{
+		public QuitButtonExample ()
+		{
+			WindowTitle = "Quit Button";
+			Resize (250, 150);
+			Move (300, 300);
+
+			InitUI ();
+
+			Show ();
+		}
+
+		private void InitUI()
+		{
+			QPushButton button = new QPushButton ("Quit", this);
+			button.SetGeometry (50, 40, 80, 20);
+			button.Clicked += (obj) => {
+				QApplication.Quit();
+			};
+		}
+
+		public static unsafe int Main(String[] args) 
+		{
+			QApplication application = Shared.QApplicationSingleton.GetQApplication(args);
+
+			new QuitButtonExample();
+
+			return QApplication.Exec();
+		}
+	}
+}
+
